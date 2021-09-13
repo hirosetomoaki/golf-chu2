d6f10c3a686ba187baa166d37e7726e3fe77626a
hirosetomoaki <tomokyun@live.jp>
2021-09-13 14:39:50 +0900

    バリデーション、ユーザー管理の残り

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
          validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
          validates :name_initials, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
+
+  has_many :items
+  has_many :buys
+  has_many :rooms, through: :room_users
+  has_many :photos
 end
