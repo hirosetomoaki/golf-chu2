9d60ab86436c7ec99efe31786939507cbeb0bd64
hirosetomoaki <tomokyun@live.jp>
2021-09-13 12:51:15 +0900

    ユーザ管理機能、編集、バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
+         validates :name_initials, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
